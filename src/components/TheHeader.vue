41b7421c17b50fedada0b6eb448e5db120513012
jules2m1998 <mevaajules9@gmail.com>
2020-06-27 00:43:05 +0100

    Creation du header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,16 +4,32 @@
                 color="primary"
                 dense
                 dark
+                height="60px"
         >
             <v-toolbar-title><img src="~@/assets/logo.svg" alt="logo" id="logo"></v-toolbar-title>
 
             <v-spacer></v-spacer>
 
-            <div id="menu"></div>
+            <div id="menu" class="mr-5 mt-3">
+                 <ul class="d-flex flex-row">
+                     <li class="d-flex flex-row mr-4" v-for="(value, index) in menu" :key="index">
+                         <div class="menu__ico">
+                             <v-icon>{{ value.ico }}</v-icon>
+                         </div>
+                         <div class="menu__title">
+                             <a href="#" class="menu--text">{{ value.title | capitalize}}</a>
+                         </div>
+                     </li>
+                 </ul>
+            </div>
             <div id="topbuttons">
-                <v-btn class="ma-2" outlined color="white">
-                    Accueil
-                    <v-icon>mdi-cloud-upload</v-icon>
+                <v-btn class="ma-2" outlined color="white" autocapitalize="characters">
+                    Inscription
+                    <v-icon right>mdi-account-plus</v-icon>
+                </v-btn>
+                <v-btn class="ma-2 text--primary" tile color="white">
+                    Connexion
+                    <v-icon right>mdi-login</v-icon>
                 </v-btn>
             </div>
         </v-app-bar>
@@ -22,7 +38,37 @@
 
 <script>
     export default {
-        name: "Header"
+        name: "Header",
+        data: () => ({
+            menu: [
+
+                {
+                    ico: 'mdi-home',
+                    title: 'Accueil',
+                    to: 'declare'
+                },
+                {
+                    ico: 'mdi-account-search',
+                    title: 'rechercher',
+                    to: 'search'
+                },
+                {
+                    ico: 'mdi-alert-box',
+                    title: 'Déclarer',
+                    to: 'declare'
+                },
+            ]
+        }),
+        filters: {
+            /***
+             * retourne le texte sous sa frome capitalisée
+             * @param value {string}
+             * @returns {string}
+             */
+            capitalize(value){
+                return value.charAt(0).toLocaleUpperCase() + value.slice(1)
+            }
+        }
     }
 </script>
 
@@ -30,5 +76,9 @@
     #logo{
         width: auto;
         height: 31px;
+    }
+    .menu--text{
+        color: white!important;
+        text-decoration: none;
     }
 </style>
