df9818bee88dd676c706a169015f9d3246798761
jules2m1998 <mevaajules9@gmail.com>
2020-06-26 23:37:31 +0100

    Init commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-app-bar
+                color="primary"
+                dense
+                dark
+        >
+            <v-toolbar-title><img src="~@/assets/logo.svg" alt="logo" id="logo"></v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <div id="menu"></div>
+            <div id="topbuttons">
+                <v-btn class="ma-2" outlined color="white">
+                    Accueil
+                    <v-icon>mdi-cloud-upload</v-icon>
+                </v-btn>
+            </div>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped lang="scss">
+    #logo{
+        width: auto;
+        height: 31px;
+    }
+</style>
